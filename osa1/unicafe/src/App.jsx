a7192d70ccaeac5203c2e441e78b6fb2a340ecab
jubiij <robot_ruttunen@hotmail.com>
2025-01-24 10:53:20 +0200

    unicafe 1.6 muokkaus paremmaksi

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -16,20 +16,39 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
+  // tallennetaan kaikki klikit totaliin
+  const [total, setTotal] = useState(0)
+
+  // Käsitellään klikkaukset hyvälle
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+
+  // Käsitellään klikkaukset neutraalille
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
       <Header />
-      <Button onClick={() => setGood(good + 1)} text="good" />
-      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button onClick={() => setBad(bad + 1)} text="bad" />
-       
-      <h2>statistics</h2>
-
+      <Button onClick={handleGoodClick} text="good"/>
+      <Button onClick={handleNeutralClick} text="neutral"/>
+      <Button onClick={handleBadClick} text="bad"/>
+      <h2>Statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      
+      <p>total {total}</p>
+      <p>average {(good - bad) / total}</p>
     </div>
   )
 }
